e918c23d295d83b40d05a94234158643d669aa38
Mohamed Ibrahim <mahmedprof@gmail.com>
2026-03-17 16:50:49 +0200

    user authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,29 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
-    expiresIn: "30d",
-  });
+  const token = await user.createToken();
+
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 const login = async (req, res) => {
-  res.send("Login User");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide an email and a password");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Password");
+  }
+  const token = await user.createToken();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs/dist/bcrypt");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,4 +30,18 @@ UserSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+UserSchema.methods.createToken = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    },
+  );
+};
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
 module.exports = mongoose.model("User", UserSchema);
